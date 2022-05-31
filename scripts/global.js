c21dffb523119df43ea4cc37165b6c8335e4cbb8
Timur Ercan <timur.ercan31@gmail.com>
2022-05-31 20:08:37 +0200

    Update global.js

--- scripts/global.js
+++ scripts/global.js
@@ -1,2 +1,2 @@
 console.log('( ͡° ͜ʖ ͡°)');
-console.log('If you are reading this you should get in touch: hi@venturoventures.com');
+console.log('If you are reading this you should get in touch: secret@venturoventures.com');
